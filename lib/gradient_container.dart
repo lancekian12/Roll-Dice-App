fb7b0de49fc671590d339d86720df4a4d7f41c73
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-07-14 12:59:56 +0800

    Styled the button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -23,16 +23,34 @@ class GradientContainer extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2],
+          colors: [
+            color1,
+            color2,
+          ],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
       child: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset('assets/images/dice-2.png', width: 200),
-            TextButton(onPressed: rollDice, child: const Text('Roll dice'))
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+              child: const Text('Roll dice'),
+            )
           ],
         ),
       ),
